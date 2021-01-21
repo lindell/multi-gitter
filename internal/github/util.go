dd18402365c0f41440bd580497cbd12e0738bc7e
Johan Lindell <johan@lindell.me>
2021-01-21 21:58:10 +0100

    feat: added --merge-type flag (#64)
    * feat: added --merge-type flag
    
    * fix: fixed typo

---
+++ internal/github/util.go
@@ -0,0 +1,28 @@
+package github
+
+import (
+	"github.com/google/go-github/v32/github"
+	"github.com/lindell/multi-gitter/internal/domain"
+)
+
+// maps merge types to what they are called in the github api
+var mergeTypeGhName = map[domain.MergeType]string{
+	domain.MergeTypeMerge:  "merge",
+	domain.MergeTypeRebase: "rebase",
+	domain.MergeTypeSquash: "squash",
+}
+
+// repoMergeTypes returns a list of all allowed merge types
+func repoMergeTypes(repo *github.Repository) []domain.MergeType {
+	ret := []domain.MergeType{}
+	if repo.GetAllowMergeCommit() {
+		ret = append(ret, domain.MergeTypeMerge)
+	}
+	if repo.GetAllowRebaseMerge() {
+		ret = append(ret, domain.MergeTypeRebase)
+	}
+	if repo.GetAllowSquashMerge() {
+		ret = append(ret, domain.MergeTypeSquash)
+	}
+	return ret
+}
