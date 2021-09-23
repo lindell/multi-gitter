a5d404e9af2f230c8ba5ac6cb51d6cc7241758ba
Tyrone Meijn <tyrone_meijn@hotmail.com>
2021-09-23 16:14:47 +0200

    add comma

--- internal/scm/gitlab/gitlab.go
+++ internal/scm/gitlab/gitlab.go
@@ -151,7 +151,7 @@ func (g *Gitlab) getGroupProjects(ctx context.Context, groupName string) ([]*git
 				Page:    i,
 			},
 			IncludeSubgroups: &g.Config.IncludeSubgroups,
-			WithMergeRequestsEnabled: true
+			WithMergeRequestsEnabled: true,
 		}, gitlab.WithContext(ctx))
 		if err != nil {
 			return nil, err
