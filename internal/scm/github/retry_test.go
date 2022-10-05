b189c8cf9b23f2e35132250f585311e4e9575ab7
Johan Lindell <johan@lindell.me>
2022-10-05 17:52:01 +0000

    updated go-github version

--- internal/scm/github/retry_test.go
+++ internal/scm/github/retry_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-github/v39/github"
+	"github.com/google/go-github/v47/github"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
