93f28c48fdd6181eb31e823ef6a33620d1b94aa9
Dominic Evans <dominic.evans@uk.ibm.com>
2024-02-02 09:15:33 +0000

    fix(gitea): return early in setReviewers
    If no new reviewers were passed as arguments then we just leave the
    existing ones alone.
    
    Also simplify by switching to using non-interface repository for
    the param as setReviewers is an internal func to this package.

--- internal/scm/gitea/gitea.go
+++ internal/scm/gitea/gitea.go
@@ -306,26 +306,25 @@ func (g *Gitea) getLabelsFromStrings(ctx context.Context, repo repository, label
 	return ret, nil
 }
 
-func (g *Gitea) setReviewers(ctx context.Context, repo scm.Repository, newPR scm.NewPullRequest, createdPR *gitea.PullRequest) error {
-	r := repo.(repository)
-
-	var addedReviewers, removedReviewers []string
-	if newPR.Reviewers != nil {
-		reviews, _, err := g.giteaClient(ctx).ListPullReviews(r.ownerName, r.name, createdPR.Index, gitea.ListPullReviewsOptions{})
-		if err != nil {
-			return errors.Wrap(err, "could not list existing reviews on pull request")
-		}
-		reviews = slices.DeleteFunc(reviews, func(review *gitea.PullReview) bool {
-			return review.State != gitea.ReviewStateRequestReview // can only remove reviews in requested state
-		})
-		existingReviewers := scm.Map(reviews, func(review *gitea.PullReview) string {
-			return review.Reviewer.UserName
-		})
-		addedReviewers, removedReviewers = scm.Diff(existingReviewers, newPR.Reviewers)
+func (g *Gitea) setReviewers(ctx context.Context, repo repository, newPR scm.NewPullRequest, createdPR *gitea.PullRequest) error {
+	if newPR.Reviewers == nil {
+		return nil
 	}
 
+	reviews, _, err := g.giteaClient(ctx).ListPullReviews(repo.ownerName, repo.name, createdPR.Index, gitea.ListPullReviewsOptions{})
+	if err != nil {
+		return errors.Wrap(err, "could not list existing reviews on pull request")
+	}
+	reviews = slices.DeleteFunc(reviews, func(review *gitea.PullReview) bool {
+		return review.State != gitea.ReviewStateRequestReview // can only remove reviews in requested state
+	})
+	existingReviewers := scm.Map(reviews, func(review *gitea.PullReview) string {
+		return review.Reviewer.UserName
+	})
+	addedReviewers, removedReviewers := scm.Diff(existingReviewers, newPR.Reviewers)
+
 	if len(addedReviewers) > 0 {
-		_, err := g.giteaClient(ctx).CreateReviewRequests(r.ownerName, r.name, createdPR.Index, gitea.PullReviewRequestOptions{
+		_, err := g.giteaClient(ctx).CreateReviewRequests(repo.ownerName, repo.name, createdPR.Index, gitea.PullReviewRequestOptions{
 			Reviewers: addedReviewers,
 		})
 		if err != nil {
@@ -334,7 +333,7 @@ func (g *Gitea) setReviewers(ctx context.Context, repo scm.Repository, newPR scm
 	}
 
 	if len(removedReviewers) > 0 {
-		_, err := g.giteaClient(ctx).DeleteReviewRequests(r.ownerName, r.name, createdPR.Index, gitea.PullReviewRequestOptions{
+		_, err := g.giteaClient(ctx).DeleteReviewRequests(repo.ownerName, repo.name, createdPR.Index, gitea.PullReviewRequestOptions{
 			Reviewers: removedReviewers,
 		})
 		if err != nil {
