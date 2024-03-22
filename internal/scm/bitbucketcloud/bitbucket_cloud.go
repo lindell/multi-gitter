ed6e44bfbc4751598e212b0274a4fbfa38d8e914
gcase555 <g@devnull.com>
2024-03-22 16:12:10 -0500

    adding init methods

--- internal/scm/bitbucketcloud/bitbucket_cloud.go
+++ internal/scm/bitbucketcloud/bitbucket_cloud.go
@@ -1,1 +1,48 @@
 package bitbucketcloud
+
+import (
+    "context"
+    "github.com/lindell/multi-gitter/internal/scm"
+)
+
+type BitbucketCloud struct{}
+
+func (bc *BitbucketCloud) GetRepositories(ctx context.Context) ([]scm.Repository, error) {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) CreatePullRequest(ctx context.Context, repo scm.Repository, prRepo scm.Repository, newPR scm.NewPullRequest) (scm.PullRequest, error) {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) UpdatePullRequest(ctx context.Context, repo scm.Repository, pullReq scm.PullRequest, updatedPR scm.NewPullRequest) (scm.PullRequest, error) {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) GetPullRequests(ctx context.Context, branchName string) ([]scm.PullRequest, error) {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) GetOpenPullRequest(ctx context.Context, repo scm.Repository, branchName string) (scm.PullRequest, error) {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) MergePullRequest(ctx context.Context, pr scm.PullRequest) error {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) ClosePullRequest(ctx context.Context, pr scm.PullRequest) error {
+    //TODO implement me
+    panic("implement me")
+}
+
+func (bc *BitbucketCloud) ForkRepository(ctx context.Context, repo scm.Repository, newOwner string) (scm.Repository, error) {
+    //TODO implement me
+    panic("implement me")
+}
