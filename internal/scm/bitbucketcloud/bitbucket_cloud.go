08e2f995a94a395d77fdf9c7b7423c198cacd23d
gcase555 <g@devnull.com>
2024-03-25 11:08:39 -0500

    added go-bitbuckety

--- internal/scm/bitbucketcloud/bitbucket_cloud.go
+++ internal/scm/bitbucketcloud/bitbucket_cloud.go
@@ -2,6 +2,7 @@ package bitbucketcloud
 
 import (
     "context"
+    "github.com/ktrysmt/go-bitbucket"
     "github.com/lindell/multi-gitter/internal/scm"
 )
 
@@ -9,6 +10,20 @@ type BitbucketCloud struct{}
 
 func (bc *BitbucketCloud) CreatePullRequest(ctx context.Context, repo scm.Repository, prRepo scm.Repository, newPR scm.NewPullRequest) (scm.PullRequest, error) {
     //TODO implement me
+    //ctoken := bitbucket.New
+    c := bitbucket.NewBasicAuth("username", "password")
+
+    opt := &bitbucket.PullRequestsOptions{
+        Owner:             "your-team",
+        RepoSlug:          "awesome-project",
+        SourceBranch:      "develop",
+        DestinationBranch: "master",
+        Title:             "fix bug. #9999",
+        CloseSourceBranch: true,
+    }
+
+    _, _ = c.Repositories.PullRequests.Create(opt)
+
     panic("implement me")
 }
 
