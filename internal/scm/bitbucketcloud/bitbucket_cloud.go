ce8d671bc89c9c6f49fd812ccf4cf57b49294c54
gcase555 <g@devnull.com>
2024-04-09 07:42:37 -0600

    updating settings for client

--- internal/scm/bitbucketcloud/bitbucket_cloud.go
+++ internal/scm/bitbucketcloud/bitbucket_cloud.go
@@ -20,31 +20,10 @@ type BitbucketCloud struct{
 func New(username string, bearerToken string, repositories *bitbucket.Repositories, workspaces *bitbucket.Workspace, sshAuth bool)(BitbucketCloud, error){
     //TODO add logic to create client here and populate it with the values present here
     return BitbucketCloud{}, nil
-    //type Client struct {
-    //    Auth         *auth
-    //    Users        users
-    //    User         user
-    //    Teams        teams
-    //    Repositories *Repositories
-    //    Workspaces   *Workspace
-    //    Pagelen      int
-    //    MaxDepth     int
-    //    // LimitPages limits the number of pages for a request
-    //    //	default value as 0 -- disable limits
-    //    LimitPages int
-    //    // DisableAutoPaging allows you to disable the default behavior of automatically requesting
-    //    // all the pages for a paginated response.
-    //    DisableAutoPaging bool
-    //
-    //    HttpClient *http.Client
-    //    // contains filtered or unexported fields
-    //}
 }
 
-func (bc *BitbucketCloud) CreatePullRequest(ctx context.Context, repo scm.Repository, prRepo scm.Repository, newPR scm.NewPullRequest) (scm.PullRequest, error) {
-    //TODO implement me
-    //ctoken := bitbucket
-    c := bitbucket.NewBasicAuth("username", "password")
+func (bbc *BitbucketCloud) CreatePullRequest(ctx context.Context, repo scm.Repository, prRepo scm.Repository, newPR scm.NewPullRequest) (scm.PullRequest, error) {
+    //TODO update to use configured settings and actually handle responses
 
     opt := &bitbucket.PullRequestsOptions{
         Owner:             "your-team",
@@ -55,42 +34,42 @@ func (bc *BitbucketCloud) CreatePullRequest(ctx context.Context, repo scm.Reposi
         CloseSourceBranch: true,
     }
 
-    _, _ = c.Repositories.PullRequests.Create(opt)
+    _, _ = bbc.bbClient.Repositories.PullRequests.Create(opt)
 
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) UpdatePullRequest(ctx context.Context, repo scm.Repository, pullReq scm.PullRequest, updatedPR scm.NewPullRequest) (scm.PullRequest, error) {
+func (bbc *BitbucketCloud) UpdatePullRequest(ctx context.Context, repo scm.Repository, pullReq scm.PullRequest, updatedPR scm.NewPullRequest) (scm.PullRequest, error) {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) GetPullRequests(ctx context.Context, branchName string) ([]scm.PullRequest, error) {
+func (bbc *BitbucketCloud) GetPullRequests(ctx context.Context, branchName string) ([]scm.PullRequest, error) {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) GetOpenPullRequest(ctx context.Context, repo scm.Repository, branchName string) (scm.PullRequest, error) {
+func (bbc *BitbucketCloud) GetOpenPullRequest(ctx context.Context, repo scm.Repository, branchName string) (scm.PullRequest, error) {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) MergePullRequest(ctx context.Context, pr scm.PullRequest) error {
+func (bbc *BitbucketCloud) MergePullRequest(ctx context.Context, pr scm.PullRequest) error {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) ClosePullRequest(ctx context.Context, pr scm.PullRequest) error {
+func (bbc *BitbucketCloud) ClosePullRequest(ctx context.Context, pr scm.PullRequest) error {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) GetRepositories(ctx context.Context) ([]scm.Repository, error) {
+func (bbc *BitbucketCloud) GetRepositories(ctx context.Context) ([]scm.Repository, error) {
     //TODO implement me
     panic("implement me")
 }
 
-func (bc *BitbucketCloud) ForkRepository(ctx context.Context, repo scm.Repository, newOwner string) (scm.Repository, error) {
+func (bbc *BitbucketCloud) ForkRepository(ctx context.Context, repo scm.Repository, newOwner string) (scm.Repository, error) {
     //TODO implement me
     panic("implement me")
 }
